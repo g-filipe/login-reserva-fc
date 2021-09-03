ea5265be3f807c0fb6c4cc0a463127702fff6aad
Daiane <daianemarcon@gmail.com>
2021-09-02 22:35:54 -0300

    teste de conexao com banco e js

--- server.php
+++ server.php
@@ -1,4 +1,53 @@
 <?php
+
+
+    /**
+     * só um get aleatorio pra testar se a interação com banco e js ta funcionando direito
+     */
+
+$op = $_GET["op"];
+    switch ($op) {
+        case 'read':
+            echo fnRead();
+        default:
+            break;
+    }
+
+    /*/
+     * lê dados do bd
+     */
+    function fnRead(){
+        $connection = connect();
+        $sql = "SELECT * FROM USUARIO";
+        $result = mysqli_query($connection, $sql);
+        $json = generateJson($result);
+        disconnect($connection);
+        return $json;
+    }
+
+    /**
+     * gera o json
+     */
+    function generateJson($result){
+        $json = "";
+        while ($line = mysqli_fetch_array($result, MYSQLI_ASSOC)){
+            $id_usuario = isset($line["id_usuario"]) ? $line["id_usuario"] : null;
+            $nome = isset($line["nome"]) ? $line["nome"] : null;
+            $email = isset($line["email"]) ? $line["email"] : null;
+            $senha = isset($line["senha"]) ? $line["senha"] : null;
+
+
+            $json .= "{\"id_usuario\":\"".$id_usuario."\",
+                    \"nome\":\"".$nome."\", 
+                    \"email\":\"".$email."\", 
+                    \"senha\":\"".$senha."\"
+                     },";
+        }
+        $json = substr($json, 0, strlen($json) - 1);
+        $json = "[" . $json. "]";
+        return $json;
+    }
+
     /**
      * conecta no banco de dados mySQL
      */
